0085547bd0d5cc30d2e594edd45c603a193bdfd7
jgw9617 <jgw9617@gmail.com>
2020-05-18 15:30:45 -0700

    feat(): browser support

--- src/script/pages/demo-detail.ts
+++ src/script/pages/demo-detail.ts
@@ -210,6 +210,45 @@ export class DemoDetail extends LitElement {
         align-items: start;
       }
 
+      #support {
+        display: flex;
+        flex-direction: column;
+
+        background: white;
+        width: 56%;
+        padding-right: 1em;
+        margin-left: 9em;
+        margin-top: 2em;
+        padding-left: 1em;
+        padding-bottom: 1em;
+        border-radius: 8px;
+      }
+
+      #support img {
+        height: 2em;
+        width: 2em;
+      }
+
+      #supportDetails {
+        display: flex;
+      }
+
+      .supportBlock {
+        display: flex;
+        align-items: center;
+
+        width: 8em;
+        background: #f0f0f0;
+        border-radius: 8px;
+        padding-top: 6px;
+        padding-bottom: 6px;
+        padding-left: 16px;
+        padding-right: 8px;
+        justify-content: space-between;
+        margin-right: 8px;
+        font-weight: bold;
+      }
+
       @media(max-width: 800px) {
         #headerBlock, #demo, #readme {
           margin-left: 0;
@@ -314,6 +353,28 @@ export class DemoDetail extends LitElement {
         ${!this.demo?.video_url ? html`<section id="demo">
           <img .src="${this.demo?.screenshot_url}">
         </section>` : null}
+
+        ${
+          this.demo?.support ? html`
+            <section id="support">
+              <h3>Browser Support</h3>
+
+              <div id="supportDetails">
+                ${
+                  this.demo?.support.map((platform: any) => {
+                    return html`
+                      <div class="supportBlock">
+                        ${platform.browser}
+
+                        ${platform.support ? html`<img src="/assets/check.svg" alt="checked">` : html`<img src="/assets/close.svg" alt="no support">`}
+                      </div>
+                    `
+                  })
+                }
+              </div>
+            </section>
+          ` : null
+        }
 
         ${this.readme ? html`<section id="readme" .innerHTML="${this.readme}"></section>` : null}
       </div>
