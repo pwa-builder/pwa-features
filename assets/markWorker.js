4d68f5fe6531ea8d5b7cfe1b2fcc59b88b1d4d77
Justin Willis <jgw9617@gmail.com>
2020-06-29 10:24:48 -0700

    feat(): start work on code highlighting

--- assets/markWorker.js
+++ assets/markWorker.js
@@ -1,8 +1,14 @@
 importScripts('/assets/marked.min.js');
+importScripts('/assets/highlight.js');
 
 onmessage = (e) => {
   const markdownString = e.data;
   postMessage(marked(markdownString, {
-    breaks: true
+    breaks: true,
+    highlight: (code, language) => {
+      const hljs = self.hljs;
+      const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+      return hljs.highlight(validLanguage, code).value;
+    },
   }));
 };
